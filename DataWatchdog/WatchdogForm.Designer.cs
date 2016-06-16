58325811a41d3f073546b01230514f62e3a7fa6a
James Huang <james_huang@imposition.com.tw>
2016-06-17 02:34:25 +0800

    Add project files.

---
+++ DataWatchdog/WatchdogForm.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace DataWatchdog
+{
+    partial class WatchdogForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(WatchdogForm));
+            this.trayIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.resetMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.autoShutdownMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.notifyMessage = new System.Windows.Forms.Label();
+            this.closeNotifyBtn = new System.Windows.Forms.Button();
+            this.shutdownBtn = new System.Windows.Forms.Button();
+            this.contextMenuStrip.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // trayIcon
+            // 
+            this.trayIcon.ContextMenuStrip = this.contextMenuStrip;
+            this.trayIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("trayIcon.Icon")));
+            this.trayIcon.Text = "Data Watchdog";
+            this.trayIcon.Visible = true;
+            // 
+            // contextMenuStrip
+            // 
+            this.contextMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.resetMenuItem,
+            this.exitMenuItem,
+            this.toolStripSeparator1,
+            this.autoShutdownMenuItem});
+            this.contextMenuStrip.Name = "contextMenuStrip";
+            this.contextMenuStrip.Size = new System.Drawing.Size(162, 98);
+            // 
+            // resetMenuItem
+            // 
+            this.resetMenuItem.Name = "resetMenuItem";
+            this.resetMenuItem.Size = new System.Drawing.Size(161, 22);
+            this.resetMenuItem.Text = "Reset";
+            this.resetMenuItem.Click += new System.EventHandler(this.resetMenuItem_Click);
+            // 
+            // exitMenuItem
+            // 
+            this.exitMenuItem.Name = "exitMenuItem";
+            this.exitMenuItem.Size = new System.Drawing.Size(161, 22);
+            this.exitMenuItem.Text = "Exit";
+            this.exitMenuItem.Click += new System.EventHandler(this.exitMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(158, 6);
+            // 
+            // autoShutdownMenuItem
+            // 
+            this.autoShutdownMenuItem.Name = "autoShutdownMenuItem";
+            this.autoShutdownMenuItem.Size = new System.Drawing.Size(161, 22);
+            this.autoShutdownMenuItem.Text = "Auto Shutdown";
+            this.autoShutdownMenuItem.Click += new System.EventHandler(this.autoShutdownMenuItem_Click);
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Image = global::DataWatchdog.Properties.Resources.watch_dog;
+            this.pictureBox.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox.TabIndex = 1;
+            this.pictureBox.TabStop = false;
+            // 
+            // notifyMessage
+            // 
+            this.notifyMessage.AutoSize = true;
+            this.notifyMessage.Location = new System.Drawing.Point(68, 13);
+            this.notifyMessage.Name = "notifyMessage";
+            this.notifyMessage.Size = new System.Drawing.Size(79, 12);
+            this.notifyMessage.TabIndex = 2;
+            this.notifyMessage.Text = "Notify message.";
+            // 
+            // closeNotifyBtn
+            // 
+            this.closeNotifyBtn.Location = new System.Drawing.Point(268, 2);
+            this.closeNotifyBtn.Name = "closeNotifyBtn";
+            this.closeNotifyBtn.Size = new System.Drawing.Size(23, 23);
+            this.closeNotifyBtn.TabIndex = 3;
+            this.closeNotifyBtn.Text = "x";
+            this.closeNotifyBtn.UseVisualStyleBackColor = true;
+            this.closeNotifyBtn.Click += new System.EventHandler(this.closeNotifyBtn_Click);
+            // 
+            // shutdownBtn
+            // 
+            this.shutdownBtn.Location = new System.Drawing.Point(205, 41);
+            this.shutdownBtn.Name = "shutdownBtn";
+            this.shutdownBtn.Size = new System.Drawing.Size(75, 23);
+            this.shutdownBtn.TabIndex = 4;
+            this.shutdownBtn.Text = "Shutdown";
+            this.shutdownBtn.UseVisualStyleBackColor = true;
+            this.shutdownBtn.Click += new System.EventHandler(this.shutdownBtn_Click);
+            // 
+            // WatchdogForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 76);
+            this.Controls.Add(this.shutdownBtn);
+            this.Controls.Add(this.closeNotifyBtn);
+            this.Controls.Add(this.notifyMessage);
+            this.Controls.Add(this.pictureBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "WatchdogForm";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "Watchdog";
+            this.TopMost = true;
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.Load += new System.EventHandler(this.WatchdogForm_Load);
+            this.contextMenuStrip.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon trayIcon;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem resetMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Label notifyMessage;
+        private System.Windows.Forms.Button closeNotifyBtn;
+        private System.Windows.Forms.Button shutdownBtn;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem autoShutdownMenuItem;
+    }
+}
+
---
+++ DataWatchdog/WatchdogForm.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Management;
+using System.Windows.Forms;
+
+namespace DataWatchdog
+{
+    public partial class WatchdogForm : Form
+    {
+        public WatchdogForm()
+        {
+            InitializeComponent();
+            autoShutdownMenuItem.Checked = Properties.Settings.Default.AutoShutdown;
+            VerifyOrInit();
+        }
+
+        private void exitMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void resetMenuItem_Click(object sender, EventArgs e)
+        {
+            Reset();
+        }
+
+        private void shutdownBtn_Click(object sender, EventArgs e)
+        {
+            Shutdown();
+            Hide();
+        }
+
+        private void closeNotifyBtn_Click(object sender, EventArgs e)
+        {
+            Hide();
+        }
+
+        private void WatchdogForm_Load(object sender, EventArgs e)
+        {
+            Hide();
+        }
+
+        private void autoShutdownMenuItem_Click(object sender, EventArgs e)
+        {
+            autoShutdownMenuItem.Checked = !autoShutdownMenuItem.Checked;
+            Properties.Settings.Default.AutoShutdown = autoShutdownMenuItem.Checked;
+            Properties.Settings.Default.Save();
+        }
+
+        private string _decoyFolderName = Properties.Settings.Default.FolderName;
+        private string _decoyFilename = Properties.Settings.Default.Filename;
+        private string _decoyContent = Properties.Settings.Default.Content;
+        private List<FileSystemWatcher> _watchers = new List<FileSystemWatcher>();
+
+        private void VerifyOrInit(bool reset = false)
+        {
+            var fixedOnly = Properties.Settings.Default.FixedOnly;
+            // Verify or init watcher folders
+            var driverList = RetrieveDrives(fixedOnly);
+            foreach (var driveName in driverList)
+            {
+                if (!VerifyOrdInitWatchFolder(driveName, reset))
+                    MessageBox.Show(string.Format("{0} not valid!", driveName), "Watch Dog", MessageBoxButtons.OK,
+                        MessageBoxIcon.Warning);
+            }
+            // Init folder watcher
+            foreach (var driveName in driverList)
+            {
+                var watcher = new FileSystemWatcher(driveName + _decoyFolderName);
+                watcher.NotifyFilter = NotifyFilters.FileName | NotifyFilters.LastWrite | NotifyFilters.Size;
+                watcher.Created += WatcherEventHandler;
+                watcher.Renamed += WatcherEventHandler;
+                watcher.Changed += WatcherEventHandler;
+                watcher.EnableRaisingEvents = true;
+                _watchers.Add(watcher);
+            }
+        }
+
+        private void WatcherEventHandler(object sender, FileSystemEventArgs fileSystemEventArgs)
+        {
+            // Sound the alarm
+            var target = fileSystemEventArgs.FullPath;
+            MessageBox.Show($"Alarm on {target}", "Watch Dog", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+
+        private bool VerifyOrdInitWatchFolder(string driveName, bool reset)
+        {
+            var watchFolder = new DirectoryInfo(driveName + _decoyFolderName);
+            if (watchFolder.Exists)
+            {
+                try
+                {
+                    if (reset)
+                    {
+                        watchFolder.Delete(true);
+                    }
+                    else
+                    {
+                        var files = watchFolder.GetFiles();
+                        // check file number
+                        if (files.Length != 1) return false;
+                        // check filename
+                        if (!files[0].Name.Equals(_decoyFilename)) return false;
+                        // check file content
+                        if (!files[0].OpenText().ReadLine().Equals(_decoyContent)) return false;
+                        return true;
+                    }
+                }
+                catch (Exception)
+                {
+                    return false;
+                }
+            }
+            // init watch folder
+            try
+            {
+                watchFolder.Create();
+                var decoyFile = new FileInfo($"{watchFolder.FullName}\\{_decoyFilename}");
+                using (var sw = new StreamWriter(decoyFile.Create()))
+                {
+                    for (int i = 0; i < 1000; i++)
+                    {
+                        sw.WriteLine(_decoyContent);
+                    }
+                    sw.Close();
+                }
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private List<string> RetrieveDrives(bool fixedOnly)
+        {
+            var driveList = new List<string>();
+            var drives = DriveInfo.GetDrives();
+            foreach (var driveInfo in drives)
+            {
+                var driveName = driveInfo.Name;
+                switch (driveInfo.DriveType)
+                {
+                    case System.IO.DriveType.Fixed:
+                    case System.IO.DriveType.Removable:
+                        if (fixedOnly)
+                        {
+                            if (!IsExternalDisk(driveName)) driveList.Add(driveName);
+                        }
+                        else
+                            driveList.Add(driveName);
+                        break;
+                    case DriveType.Unknown:
+                        break;
+                    case DriveType.NoRootDirectory:
+                        break;
+                    case DriveType.Network:
+                        break;
+                    case DriveType.CDRom:
+                        break;
+                    case DriveType.Ram:
+                        break;
+                    default:
+                        break;
+                }
+            }
+            return driveList;
+        }
+
+        private bool IsExternalDisk(string driveLetter)
+        {
+            bool retVal = false;
+            driveLetter = driveLetter.TrimEnd('\\');
+
+            // browse all USB WMI physical disks
+            foreach (ManagementObject drive in new ManagementObjectSearcher("select DeviceID, MediaType,InterfaceType from Win32_DiskDrive").Get())
+            {
+                // associate physical disks with partitions
+                ManagementObjectCollection partitionCollection = new ManagementObjectSearcher(String.Format("associators of {{Win32_DiskDrive.DeviceID='{0}'}} " + "where AssocClass = Win32_DiskDriveToDiskPartition", drive["DeviceID"])).Get();
+
+                foreach (ManagementObject partition in partitionCollection)
+                {
+                    if (partition != null)
+                    {
+                        // associate partitions with logical disks (drive letter volumes)
+                        ManagementObjectCollection logicalCollection = new ManagementObjectSearcher(String.Format("associators of {{Win32_DiskPartition.DeviceID='{0}'}} " + "where AssocClass= Win32_LogicalDiskToPartition", partition["DeviceID"])).Get();
+
+                        foreach (ManagementObject logical in logicalCollection)
+                        {
+                            if (logical != null)
+                            {
+                                // finally find the logical disk entry
+                                ManagementObjectCollection.ManagementObjectEnumerator volumeEnumerator = new ManagementObjectSearcher(String.Format("select DeviceID from Win32_LogicalDisk " + "where Name='{0}'", logical["Name"])).Get().GetEnumerator();
+
+                                volumeEnumerator.MoveNext();
+
+                                ManagementObject volume = (ManagementObject)volumeEnumerator.Current;
+
+                                if (driveLetter.ToLowerInvariant().Equals(volume["DeviceID"].ToString().ToLowerInvariant()) &&
+                                    (drive["MediaType"].ToString().ToLowerInvariant().Contains("external") || drive["InterfaceType"].ToString().ToLowerInvariant().Contains("usb")))
+                                {
+                                    retVal = true;
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            return retVal;
+        }
+
+        private void ShowNotification(string message, bool showShutdown = true, bool autoShutdown = false)
+        {
+            notifyMessage.Text = message;
+            shutdownBtn.Visible = showShutdown;
+            var screen = Screen.AllScreens[0];
+            Show();
+            this.WindowState = FormWindowState.Normal;
+            Left = screen.WorkingArea.Right - Width;
+            Top = screen.WorkingArea.Bottom - Height;
+            if (autoShutdown) Shutdown();
+        }
+
+        private void Shutdown()
+        {
+            var psi = new ProcessStartInfo("shutdown", "/s /t 0");
+            psi.CreateNoWindow = true;
+            psi.UseShellExecute = false;
+            Process.Start(psi);
+        }
+
+        private void Reset()
+        {
+            ShowNotification("This is a test message!");
+        }
+    }
+}
